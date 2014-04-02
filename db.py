5fb8a9bfb475dca38fa76f5668d740e01558bd34
Tim Foerster <timhormersdorf@gmail.com>
2014-04-02 19:28:21 +0200

    code review, added helper methods

--- db.py
+++ db.py
@@ -53,222 +53,126 @@ class BotDatabase(object):
 				host=BotDatabase.__host)
 
 	def addMember(self,user):
-		try:
-			if not type(user) is tuple:
-				raise TypeError('only tuples!!111ELF') 
-			cur = BotDatabase.__con.cursor()
-			cur.execute("INSERT INTO members (number, name) VALUES (%s,%s)", user)
-			BotDatabase.__con.commit()	
-			return True
-
-		except psycopg2.DatabaseError, e: 	
-			if BotDatabase.__con:
-				BotDatabase.__con.rollback()
-			return False
+		if not type(user) is tuple:
+			raise TypeError('user must be a tuple (number, name)') 
+		return self._insert_del("INSERT INTO members (number, name) VALUES (%s,%s)", user)
 
 	def removeMember(self, user):
-		try:
-			cur = BotDatabase.__con.cursor()
-			if type(user) is int or (type(user) is str and user.isdigit()):
-				cur.execute("DELETE FROM members_commands WHERE members_id = %s", (str(user),)) 
-				cur.execute("DELETE FROM members WHERE members_id = %s ", (str(user),))
-			else:
-				cur.execute("DELETE FROM members_commands WHERE members_id = ( " +
-						"SELECT members_id FROM members WHERE name = %s " + 
-					")", (str(user),))
-				cur.execute("DELETE FROM members WHERE name = %s ", (user,))
-			BotDatabase.__con.commit()	
-			return True
-		
-		except psycopg2.DatabaseError, e: 	
-			if BotDatabase.__con:
-				BotDatabase.__con.rollback()
-			return False
+		if type(user) is int or (type(user) is str and user.isdigit()):
+			a = self._insert_del("DELETE FROM members_commands WHERE members_id = %s", (user,)) 
+			b = self._insert_del("DELETE FROM members WHERE members_id = %s ", (user,))
+		else:
+			a = self._insert_del("DELETE FROM members_commands WHERE members_id = ( " +
+					"SELECT members_id FROM members WHERE name = %s " + 
+				")", (user,))
+			b = self._insert_del("DELETE FROM members WHERE name = %s ", (user,))
+		return (False, True)[bool(a and b)]
 
 	def addMemberCommandByMemberId(self, user_id, command):
-		try:
-			if not type(command) is int and not (type(command) is str or command.isdigit()):
-				cur = BotDatabase.__con.cursor()
-				cur.execute("SELECT commands_id FROM commands " +
-					"WHERE command = %s LIMIT 1" , (command,))
-				command = cur.fetchone()
-			cur = BotDatabase.__con.cursor()
-			cur.execute("INSERT INTO members_commands (members_id,commands_id) " +
-				"VALUES ( %s , %s )", (user_id, command))
-			BotDatabase.__con.commit()
-			return True
-		
-		except psycopg2.DatabaseError, e: 	
-			if BotDatabase.__con:
-				BotDatabase.__con.rollback()
-			return False
+		if not type(command) is int and not (type(command) is str or command.isdigit()):
+			command = self._select("SELECT commands_id FROM commands WHERE command = %s LIMIT 1" , (command,) , True)
+		return self._insert_del("INSERT INTO members_commands (members_id, commands_id) VALUES ( %s , %s )", 
+			(user_id, command))
 
 	def addMemberCommandByMemberName(self, user_name, command):
-		try:
-			cur = BotDatabase.__con.cursor()
-			cur.execute("SELECT members_id FROM members " +
-				"WHERE name = %s LIMIT 1" , (user_name,))
-			user_name = cur.fetchone()
-			if self.addMemberCommandByMemberId(user_name,command):
-				return True
-			else:
-				return False
-		
-		except psycopg2.DatabaseError, e: 	
-			if BotDatabase.__con:
-				BotDatabase.__con.rollback()
-			return False
+		user_name = self._select("SELECT members_id FROM members " +
+			"WHERE name = %s LIMIT 1" , (user_name,), True)
+		return(False, True)[bool(self.addMemberCommandByMemberId(user_name,command))]
 
 	def getCommands(self):
-		try:
-			cur = BotDatabase.__con.cursor()
-			cur.execute("SELECT * FROM commands")
-			return cur.fetchall()
-
-		except psycopg2.DatabaseError, e: 	
-			if BotDatabase.__con:
-				BotDatabase.__con.rollback()
-			return False
+		return self._select("SELECT * FROM commands",False,False)
 	
 	def addCommand(self,dataset):
-		try:
-			if type(dataset) is tuple and len(dataset) == 3:
-				cur = BotDatabase.__con.cursor()
-				cur.execute("INSERT INTO commands (command, short_hint, description) "+
-					"VALUES (%s,%s,%s)", dataset)
-				BotDatabase.__con.commit()
-			else:
-				raise TypeError('i requested a tuple with len == 3!!!111ELF')
-		
-		except psycopg2.DatabaseError, e: 	
-			if BotDatabase.__con:
-				BotDatabase.__con.rollback()
-			return False
+		if type(dataset) is tuple and len(dataset) == 3:
+			return self._insert_del("INSERT INTO commands (command, short_hint, description) VALUES (%s,%s,%s)", dataset)
+		else:
+			raise TypeError('tuple needed (command, short_hint, descrition)')
 	
 	def removeCommand(self,command):
-		try:
-			cur = BotDatabase.__con.cursor()
-			if type(command) is int or (type(command) is str and command.isdigit()):
-				cur.execute("DELETE FROM members_commands WHERE commands_id = %s", (str(command),)) 
-				cur.execute("DELETE FROM commands WHERE commands_id = %s ", (str(command),))
-			else:
-				cur.execute("DELETE FROM members_commands WHERE commands_id = ( " +
-						"SELECT commands_id FROM commands WHERE command = %s " + 
-					")", (str(command),))
-				cur.execute("DELETE FROM commands WHERE command = %s ", (command,))
-			BotDatabase.__con.commit()	
-			return True	
-	
-		except psycopg2.DatabaseError, e: 	
-			if BotDatabase.__con:
-				BotDatabase.__con.rollback()
-			return False
+		if type(command) is int or (type(command) is str and command.isdigit()):
+			a = self._insert_del("DELETE FROM members_commands WHERE commands_id = %s", (command,)) 
+			b = self._insert_del("DELETE FROM commands WHERE commands_id = %s ", (command,))
+		else:
+			a = self._insert_del("DELETE FROM members_commands WHERE commands_id = ( " +
+					"SELECT commands_id FROM commands WHERE command = %s " + 
+				")", (command,))
+			b = self._insert_del("DELETE FROM commands WHERE command = %s ", (command,))
+		return (False, True)[bool(a and b)]
 
 	def getHintByCommand(self,command):
-		try:
-			cur = BotDatabase.__con.cursor()
-			cur.execute("SELECT short_hint FROM commands WHERE command = %s ", (command,))
-			buf = cur.fetchone()
-			return (False , buf[0])[bool(buf[0])]
+		buf = self._select("SELECT short_hint FROM commands WHERE command = %s ", (command,), True)
+		return (False , buf[0])[bool(buf[0])]
 			
-		except psycopg2.DatabaseError, e: 	
-			if BotDatabase.__con:
-				BotDatabase.__con.rollback()
-			return False
-
 	def getDescriptionByCommand(self,command):
-		try:
-			cur = BotDatabase.__con.cursor()
-			cur.execute("SELECT description FROM commands WHERE command = %s ", (command,))
-			buf = cur.fetchone()
-			return (False , buf[0])[bool(buf[0])]
-						
-
-		except psycopg2.DatabaseError, e: 	
-			if BotDatabase.__con:
-				BotDatabase.__con.rollback()
-			return False
+		buf = self._select("SELECT description FROM commands WHERE command = %s ", (command,),True)
+		return (False , buf[0])[bool(buf[0])]
 
 	def getTasksByMemberId(self,user_id):
-		try:
-			cur = BotDatabase.__con.cursor()
-			cur.execute("SELECT * FROM tasks WHERE members_id = %s ", (user_id,))
-			return cur.fetchall()
-			
-		except psycopg2.DatabaseError, e: 	
-			if BotDatabase.__con:
-				BotDatabase.__con.rollback()
-			return False
+		return self._select("SELECT * FROM tasks WHERE members_id = %s ", (user_id,), False)
 
 	def getMembers(self):
-		try:
-			cur = BotDatabase.__con.cursor()
-			cur.execute("SELECT * FROM members")
-			return cur.fetchall()
-
-		except psycopg2.DatabaseError, e: 	
-			if BotDatabase.__con:
-				BotDatabase.__con.rollback()
-			return False
+		return self._select("SELECT * FROM members", False, False)
 
 	def getMemberCommandsByMemberId(self,user_id):
-		try:
-			cur = BotDatabase.__con.cursor()
-			retTuple = tuple()
-			cur.execute(
-				"SELECT command " + 
-				"FROM commands " + 
-				"WHERE commands_id IN ( " + 
-					"SELECT commands_id FROM members_commands WHERE members_id = %s " + 
-				")", (user_id,)
-			)
-			result = cur.fetchall()
-			if not result:
-				return False
-			for i in result:
-				retTuple += (i[0],)
-			return retTuple
-			
-		except psycopg2.DatabaseError, e: 	
-			if BotDatabase.__con:
-				BotDatabase.__con.rollback()
+		retTuple = tuple()
+		result = self._select("SELECT command FROM commands WHERE commands_id IN ( " + 
+				"SELECT commands_id FROM members_commands WHERE members_id = %s " + 
+			")", (user_id,) , False)
+		if not result:
 			return False
+		for i in result:
+			retTuple += (i[0],)
+		return retTuple
 
 	def getMemberCommandsByMemberNames(self,user_names):
-		try:
-			if type(user_names) is str:
-				user_names = (user_names,)
-			elif not type(user_names) is tuple:
-				raise TypeError('tuple , Monkey !!')
-			retDict = dict()
-			cur = BotDatabase.__con.cursor()
-			for i in user_names:
-				cur.execute("SELECT members_id FROM members WHERE name = %s ", (i,))
-				buf=self.getMemberCommandsByMemberId(cur.fetchone())
-				if buf:
-					retDict[i[0]] = buf
-			return retDict
+		if type(user_names) is str:
+			user_names = (user_names,)
+		elif not type(user_names) is tuple:
+			raise TypeError('tuple , Monkey !!')
+		retDict = dict()
+		for i in user_names:
+			res = self._select("SELECT members_id FROM members WHERE name = %s ", (i,), True)
+			buf=self.getMemberCommandsByMemberId(res)
+			if buf:
+				retDict[i[0]] = buf
+		return retDict
 		
-		except psycopg2.DatabaseError, e: 	
-			if BotDatabase.__con:
-				BotDatabase.__con.rollback()
-			return False
-	
 	def getMembersWithCommands(self):
-		try:	
-			buf = tuple()
+		buf = tuple()
+		result = self._select("SELECT name FROM members", False, False)
+		if not result:
+			return False
+		for i in result:
+			 buf += (i,)
+		return self.getMemberCommandsByMemberNames(buf)
+
+	def _select (self, query, values, onlyOneResult ):
+		try:
 			cur = BotDatabase.__con.cursor()
-			cur.execute("SELECT name FROM members")
-			result = cur.fetchall()
-			if not result:
-				return False
-			for i in result:
-				 buf += (i,)
-			return self.getMemberCommandsByMemberNames(buf)
-		
+			if values:
+				cur.execute(query, values)
+			else:
+				cur.execute(query) 
+			if onlyOneResult:
+				return cur.fetchone()
+			else:
+				return cur.fetchall()
+	
 		except psycopg2.DatabaseError, e: 	
 			if BotDatabase.__con:
 				BotDatabase.__con.rollback()
 			return False
 
+	def _insert_del(self, query , values):
+		try:
+			cur = BotDatabase.__con.cursor()
+			cur.execute(query, values) 
+			result=BotDatabase.__con.commit()	
+			return True
+	
+		except psycopg2.DatabaseError, e: 	
+			if BotDatabase.__con:
+				BotDatabase.__con.rollback()
+			return False
+						
 
