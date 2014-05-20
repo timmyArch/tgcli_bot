882dc2acfd910cf8b2fa9a250e09f57245c3f637
Tim Foerster <timhormersdorf@gmail.com>
2014-05-20 23:17:54 +0200

    improved lua fif handling

--- bot.lua
+++ bot.lua
@@ -2,8 +2,10 @@ function on_msg_receive (msg)
 	if not (msg == nil) then
 		sender =""
 		message = msg.text
-		message = string.gsub(message, "\'" , "") 
-		message = string.gsub(message, "\"" , "") 
+		if message then
+			message = string.gsub(message, "\'" , "") 
+			message = string.gsub(message, "\"" , "") 
+		end
 		number = 0 
 		if not msg.from.phone == nil then
 			number = msg.from.phone
@@ -18,7 +20,7 @@ function on_msg_receive (msg)
 			sender = msg.from.print_name
 			sender_2 = sender
 		end
-		if sender == "TimmyServer" then
+		if not sender or sender == "TimmyServer" then
 			return
 		end         
 		local value = io.popen("python /var/www/tgcli_bot/bot.py -n " .. sender .. " -s " .. sender_2 .." -m '".. message.."'" )
@@ -36,36 +38,50 @@ function on_msg_receive (msg)
 end
 
 function on_second_scheduler_end ()
-	for variable = 0, 10, 1 do
-		local file = io.popen('FILE=/tmp/luabot.tmp; head -n1 $FILE && sed -i "1d" $FILE')
-		local output = file:read('*all')
-		repicent, message = output:match("([^,]+),([^,]+)")
-		if not repicent or not message or repicent == "" or message == "" then
-			return
+	local lockfile='/tmp/luabot.lock'
+	if not file_exists(lockfile) then 
+		os.execute("touch "..lockfile)
+		for variable = 1, 9, 1 do
+			local file = io.popen('FILE=/tmp/luabot.tmp; head -n1 $FILE && sed -i "1d" $FILE')
+			if file then
+				os.execute("sleep 0.1")
+				local output = file:read('*all')
+				repicent, message = output:match("([^,]+),([^,]+)")
+				if not repicent or not message or repicent == "" or message == "" then
+					os.remove(lockfile)
+					return
+				end
+				rep = repicent:match("{{photo}}([^,]+)")
+				if rep then
+					a = message:match("([A-z/0-9]+\.gif)")
+					if a then
+						send_photo (rep,a)
+					end
+					a = message:match("([A-z/0-9]+\.png)")
+					if a then
+						send_photo (rep,a)
+					end
+					a = message:match("([A-z/0-9]+\.jpeg)")
+					if a then
+						send_photo (rep,a)
+					end
+					a = message:match("([A-z/0-9]+\.jpg)")
+					if a then
+						send_photo (rep,a)
+					end
+				else
+					send_msg (repicent,message)
+				end
+				file:close()
+				os.remove(lockfile)
+			end
 		end
-		rep = repicent:match("{{photo}}([^,]+)")
-		if rep then
-			a = message:match("([A-z/0-9]+\.gif)")
-			if a then
-				send_photo (rep,a)
-			end
-			a = message:match("([A-z/0-9]+\.png)")
-			if a then
-				send_photo (rep,a)
-			end
-			a = message:match("([A-z/0-9]+\.jpeg)")
-			if a then
-				send_photo (rep,a)
-			end
-			a = message:match("([A-z/0-9]+\.jpg)")
-			if a then
-				send_photo (rep,a)
-			end
-		else
-			send_msg (repicent,message)
-		end
-		file:close()
 	end
+end
+
+function file_exists(name)
+   local f=io.open(name,"r")
+   if f~=nil then io.close(f) return true else return false end
 end
 
 function on_our_id (id)
